63f925accc78916d4c9cf986c9c084314b67d72a
Gustavo <desarrollador.gustavo@gmail.com>
2024-09-13 15:35:58 -0500

    Back-end Usuario

---
+++ src/main/java/com/emazon/user_v1/UserV1Application.java
@@ -0,0 +1,12 @@
+package com.emazon.user_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserV1Application.class, args);
+	}
+}
---
+++ src/test/java/com/emazon/user_v1/UserV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emazon.user_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
